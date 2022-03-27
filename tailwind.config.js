b0960b885a28c2d028185db5f67a255e7ba058b2
rayhan926 <rayhanbd7075@gmail.com>
2022-03-28 02:01:18 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "lufga-regular": ["Lufga-Regular"],
+        "lufga-semibold": ["Lufga-Semibold"],
+        "lufga-bold": ["Lufga-Bold"],
+        inter: ["Inter", "sans-serif"]
+      },
+      colors: {
+        blue: "#2E27FE",
+        purple: "#9A12FF",
+        gray: "#A3A5C1"
+      }
+    },
+  },
+  plugins: [],
+}
